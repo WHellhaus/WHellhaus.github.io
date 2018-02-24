cae22ca93693a8d9d11c5e90fc6657f9a905eb33
WHellhaus <wolfgang.hellicksons@gmail.com>
2018-02-23 21:32:36 -0800

    some fixes and more buttons

--- js/albums.js
+++ js/albums.js
@@ -1,7 +1,7 @@
 var saturationReviewElement = document.getElementById('saturationReview'),
     saturationReview = saturationReviewElement.innerHTML;
-var isARealBoyReviewElement = document.getElementById('isARealBoyReview'),
-    isARealBoyReview = isARealBoyReviewElement.innerHTML;
+var isARealBoyReviewElement = document.getElementById('isarealboyReview'),
+    isarealboyReview = isARealBoyReviewElement.innerHTML;
 
 var albums = [
         {
@@ -9,12 +9,16 @@ var albums = [
                 "artist": "Brockhampton",
                 "image": "https://t2.genius.com/unsafe/300x0/https%3A%2F%2Fimages.genius.com%2F63f4ae252959cf8e0fbbee735bba6bcd.1000x1000x1.jpg",
                 "reviewDate": "2/11/2018",
-                "review": saturationReview
+                "review": saturationReview,
+                "apple": "https://itunes.apple.com/ca/album/saturation/1245319817",
+                "spotify": "https://open.spotify.com/album/67smHJOf5YlFwad6dAlppm"
         },
         {
                  "title":"...Is a Real Boy",
                  "artist":"Say Anything",
                  "image":"https://www.scenepointblank.com/assets/img/covers/med_Say_Anything_Is_a_Real_Boy.png",
                  "reviewDate": "2/28/2018",
-                 "review": isARealBoyReview  
+                 "review": isarealboyReview,  
+                 "apple": "https://itunes.apple.com/us/album/is-a-real-boy/1254484325",
+                 "spotify": "https://open.spotify.com/album/2dEa0qjnAkSjGhD4wY16QT"
         }];
--- js/buildHTML.js
+++ js/buildHTML.js
@@ -5,7 +5,7 @@ function buildHtml() {
 	var html="";
 
 	for(var i=0; i<albums.length; i++) {
-        	html +="<div class=\"col l3 m6 s12 gallery-item gallery-expand gallery-filter polygon\">";
+        	html+="<div class=\"col l3 m6 s12 gallery-item gallery-expand gallery-filter polygon\">";
                 html+="    <div class=\"gallery-curve-wrapper\">";
                 html+="      <a class=\"gallery-cover gray\">";
                 html+="        <img class=\"responsive-img\" src=\""+ albums[i].image + "\" alt=\"placeholder\">";
@@ -19,10 +19,19 @@ function buildHtml() {
                 html+="          <span class=\"price\">" + albums[i].artist + "</span>";
                 html+="          <span class=\"price right\">" + "Reviewed on " + albums[i].reviewDate + "</span>";
                 html+="        </div>";
+                html+="</br>"
                 html+= albums[i].review;
-                html+="</div>"
-                html+="</div>"
-                html+="</div>"
+                html+= "<div>"
+                html+= "<a href=\"" + albums[i].apple + "\" target=\"_blank\" class=\"inline-block\">";
+                html+= "<img src=\"http://www.madduckband.com/imgs/badges/itunes_logo.png\" style=\"width:142px;height:53px;\">";
+                html+= "</a>";
+                html+= "<a href=\"" + albums[i].spotify + "\" target=\"_blank\" class=\"inline-block\">";
+                html+= "<img src=\"https://imperialnorth.se/wp-content/uploads/2017/03/badge-spotify-3.png\" style=\"width:142px;height:53px;\">";
+                html+= "</a>";
+                html+= "</div>"
+                html+= "</div>";
+                html+= "</div>";
+                html+= "</div>";
 	}
 
 	div.innerHTML = html;
